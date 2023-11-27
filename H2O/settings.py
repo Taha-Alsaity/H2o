dfaa8a80f03a16e05bd8e1aade4c2fca90832af1
taha <tahadidanothe3@gmail.com>
2023-11-28 00:34:06 +0200

    'finalv2'

--- H2O/settings.py
+++ H2O/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t46g$54=+=7hq&$_avpy)fze4m*oq1ie(x3!4=p^bc1mj)bv^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
